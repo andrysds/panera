7ecacfaba83561d2386fe45b5b570e4a468f75be
Andrys Daniel Silalahi <andrysds@gmail.com>
2018-07-31 17:56:15 +0700

    use strconv

--- handler/default.go
+++ handler/default.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"strconv"
+
 	"github.com/andrysds/panera/helper"
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -9,6 +11,6 @@ func HandleDefault(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	defaultMessage := "Hello World! Panera here."
 	message := tgbotapi.NewMessage(update.Message.Chat.ID, defaultMessage)
 	helper.SendMessage(bot, message)
-	message = tgbotapi.NewMessage(update.Message.Chat.ID, string(update.Message.Chat.ID))
+	message = tgbotapi.NewMessage(update.Message.Chat.ID, strconv.FormatInt(update.Message.Chat.ID, 10))
 	helper.SendMessage(bot, message)
 }
