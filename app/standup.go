4a5c98ab42e0621592953982662c0e8001ba1b11
Andrys Daniel Silalahi <andrysds@gmail.com>
2018-08-01 12:44:07 +0700

    send to chat instead

--- app/standup.go
+++ app/standup.go
@@ -4,6 +4,6 @@ import "gopkg.in/telegram-bot-api.v4"
 
 func (p *Panera) HandleStandup(update *tgbotapi.Update) {
 	messageText := "Yuk stand up! Yang dapat giliran untuk memimpin stand up hari ini adalah <b>Yohanes</b>"
-	message := tgbotapi.NewMessage(p.ChatId, messageText)
+	message := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
 	p.SendMessage(message)
 }
