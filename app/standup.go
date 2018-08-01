78db614059256f0c9723b10628cb8b4a4b5abcbc
Andrys Daniel Silalahi <andrysds@gmail.com>
2018-08-01 12:48:31 +0700

    remove not works bold markdown

--- app/standup.go
+++ app/standup.go
@@ -3,7 +3,7 @@ package app
 import "gopkg.in/telegram-bot-api.v4"
 
 func (p *Panera) HandleStandup(update *tgbotapi.Update) {
-	messageText := "Yuk stand up! Yang dapat giliran untuk memimpin stand up hari ini adalah <b>Yohanes</b>"
+	messageText := "Yuk stand up! Yang dapat giliran untuk memimpin stand up hari ini adalah Yohanes"
 	message := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
 	p.SendMessage(message)
 }
