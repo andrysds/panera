aae0a242967abeab3415b00a2c61c3eadc5bd597
Andrys Daniel Silalahi <andrysds@gmail.com>
2018-08-15 23:07:58 +0700

    Revert "Revert "global handler""
    This reverts commit 0aefbbf9dc45f55f3157d116ea3a8f0b3f0b48e8.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/andrysds/panera/config"
 	"github.com/andrysds/panera/cron"
 	"github.com/andrysds/panera/db"
+	"github.com/andrysds/panera/handler"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func main() {
 
 	config.Init()
 	db.InitRedis()
+	handler.Init()
 	app.Init()
 	cron.Init()
 
